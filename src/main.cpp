0ef072846f5015631331ac03cc160ea6ed1a36d4
Pedro Soares <pmbs.123@gmail.com>
2023-04-02 21:53:15 +0200

    [feat] Create a simple blynk program

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <Arduino.h>
 
-void setup() {}
+#define LED_BUILTIN 19
 
-void loop() {}
+void setup() { pinMode(LED_BUILTIN, OUTPUT); }
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
