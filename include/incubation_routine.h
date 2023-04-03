d5c7fa1aff721e6cc15d58398957c2e0e5135f0a
Pedro Soares <pmbs.123@gmail.com>
2023-04-03 21:03:45 +0200

    [feat] Create empty file for the incubation routine

---
+++ include/incubation_routine.h
@@ -0,0 +1,28 @@
+/*
+ * EggCubator - Arduino-based egg incubator controller
+ * Copyright (C) 2023 Pedro Soares
+ * See end of the file for extended copyright information
+ */
+
+#ifndef INCUBATION_ROUTINE_H
+#define INCUBATION_ROUTINE_H
+
+class IncubationRoutine {};
+
+#endif  // !INCUBATION_ROUTINE_H
+
+/*
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
