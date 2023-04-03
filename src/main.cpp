8a353f866b93bc73175d98356858b42b17fd8ce3
Pedro Soares <pmbs.123@gmail.com>
2023-04-03 21:04:25 +0200

    [feat] Make use of the thermostat class

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,46 @@
+/*
+ * EggCubator - Arduino-based egg incubator controller
+ * Copyright (C) 2023 Pedro Soares
+ * See end of the file for extended copyright information
+ */
+
 #include <Arduino.h>
+
+#include "module/thermostat.h"
 
 #define LED_BUILTIN 19
 
-void setup() { pinMode(LED_BUILTIN, OUTPUT); }
+float temp_target = 0;
+Thermostat *thermostat;
+
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    thermostat = new Thermostat();
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+    Serial.begin(115200);
+}
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+    thermostat->routine(temp_target);
+    Serial.print("Temperature: ");
+    Serial.println(thermostat->get_temp());
 }
+
+/*
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
