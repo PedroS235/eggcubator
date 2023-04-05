635e1dfafffd288872e3f365d0e835e12e99de0b
Pedro Soares <pmbs.123@gmail.com>
2023-04-05 14:26:16 +0200

    [refactor] Improve names and add a docstring

--- include/eggcubator/egg.h
+++ include/eggcubator/egg.h
@@ -6,16 +6,29 @@
  * Egg types:
  * 0: Chicken
  * 1: Quail
- * 2: Pigeon
+ * 2: Duck
+ * 3: Turkey
+ * 4: Goose
+ * 5: Pigeon
  */
 
+/**
+ * @brief Struct representing the required information about an egg
+ *
+ * @param type: type of the egg which it represents
+ * @param incubation_days: duration of the incubation in days
+ * @param eggs_rotation_period: interval between rotations (in hours)
+ * @param start_of_motor_rotation: day of incubation when the eggs should start rotating
+ * @param end_of_motor_rotation: day of incubation when the eggs should stop rotating
+ * @param target_temp: the desired targeted temperature
+ * @param target_humd: the desired targeted humidity
+ */
 typedef struct {
     uint8_t type;
-    uint8_t incubation_period;
-    uint8_t motor_rotation_duration;
-    uint8_t motor_rotation_period;
-    uint8_t start_motor_rotation_period;  // relative to the beginging of inucabtion
-    uint8_t stop_motor_rotation_period;   // relative to the end of incubation
+    uint8_t incubation_days;
+    uint8_t eggs_rotation_period;
+    uint8_t start_of_motor_rotation;
+    uint8_t end_of_motor_rotation;
     float target_temp;
     float target_humd;
 } egg_t;
