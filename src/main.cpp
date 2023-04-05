9a1d85756d219e6c45db99dcb028973725685def
Pedro Soares <pmbs.123@gmail.com>
2023-04-05 14:27:24 +0200

    [feat] Create some global variables and add the various routines to the main loop cycle

--- src/main.cpp
+++ src/main.cpp
@@ -8,29 +8,31 @@
 #include <eggcubator/incubation_routine.h>
 #include <eggcubator/module/motor_controller.h>
 #include <eggcubator/module/thermostat.h>
+#include <eggcubator/pins.h>
 #include <eggcubator/timer.h>
 
-#include "HardwareSerial.h"
-
+// --------------------
+// - Global Variables -
+// --------------------
 float temp_target = 0;
+float humd_target = 0;
+float curr_temp = 0;
+float curr_humd = 0;
+
 Thermostat *thermostat;
-MotorController *motor_controller;
 IncubationRoutine *routine;
-eggcubator::Timer *timer;
-char curr_time[9];
 
 void setup() {
     thermostat = new Thermostat();
     routine = new IncubationRoutine();
-    timer = new eggcubator::Timer();
     Serial.begin(115200);
 }
 
 void loop() {
-    timer->update();
-    routine->routine();
     thermostat->routine(temp_target);
-    timer->print_time();
+    routine->routine();
+
+    curr_temp = thermostat->get_temp();
 }
 
 /*
