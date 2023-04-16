ccdc2726b2395cfb949b4845b8a8d75cb3224ea6
Pedro Soares <pmbs.123@gmail.com>
2023-04-16 12:25:19 +0200

    [feat] Add code to test display manager

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <Arduino.h>
+#include <eggcubator/gui/display_manager.h>
 #include <eggcubator/incubation_routine.h>
 #include <eggcubator/module/motor_controller.h>
 #include <eggcubator/module/thermostat.h>
@@ -18,21 +19,27 @@ float temp_target = 0;
 float humd_target = 0;
 float curr_temp = 0;
 float curr_humd = 0;
+const char *menu_items[] = {"Incubate", "Preheat", "Settings", "Item 4", "Item 5"};
+int i = 0;
 
 Thermostat *thermostat;
 IncubationRoutine *routine;
+DisplayManager *display_manager;
 
 void setup() {
     thermostat = new Thermostat();
     routine = new IncubationRoutine();
+    display_manager = new DisplayManager();
     Serial.begin(115200);
 }
 
 void loop() {
     thermostat->routine(temp_target);
     routine->routine();
-
     curr_temp = thermostat->get_temp();
+    display_manager->draw_incubation_status_screen();
+    i = (i + 1) % 100;
+    delay(500);
 }
 
 /*
