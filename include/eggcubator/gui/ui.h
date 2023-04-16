8826d7aaf0c6061f1fb9ab0fcd02324a8bcef22e
Pedro Soares <pmbs.123@gmail.com>
2023-04-16 12:25:30 +0200

    [feat] Create empty file for the ui

---
+++ include/eggcubator/gui/ui.h
@@ -0,0 +1,10 @@
+/*
+ * EggCubator - Arduino-based egg incubator controller
+ * Copyright (C) 2023 Pedro Soares
+ * See end of the file for extended copyright information
+ */
+
+#ifndef UI_H
+#define UI_H
+
+#endif  // !UI_H
