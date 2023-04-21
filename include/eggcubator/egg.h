c947f4a79657d47f49fc164fcbc010d781940a57
Pedro Soares <pmbs.123@gmail.com>
2023-04-21 23:09:08 +0200

    [feat] Create eggs variables

--- include/eggcubator/egg.h
+++ include/eggcubator/egg.h
@@ -8,6 +8,8 @@
 #define EGG_H
 #include <Arduino.h>
 
+#include "eggcubator/configuration.h"
+
 /*
  * Egg types:
  * 0: Chicken
@@ -38,6 +40,54 @@ typedef struct {
     float target_temp;
     float target_humd;
 } egg_t;
+
+inline egg_t chicken_egg = {EGG_CHICKEN_TYPE,
+                            EGG_CHICKEN_INCUBATION_PERIOD,
+                            EGG_CHICKEN_MOTOR_ROTATION_PERIOD,
+                            EGG_CHICKEN_START_MOTOR_ROTATION_PERIOD,
+                            EGG_CHICKEN_STOP_MOTOR_ROTATION_PERIOD,
+                            EGG_CHICKEN_TARGET_TEMP,
+                            EGG_CHICKEN_TARGET_HUMD};
+
+inline egg_t quail_egg = {EGG_QUAIL_TYPE,
+                          EGG_QUAIL_INCUBATION_PERIOD,
+                          EGG_QUAIL_MOTOR_ROTATION_PERIOD,
+                          EGG_QUAIL_START_MOTOR_ROTATION_PERIOD,
+                          EGG_QUAIL_STOP_MOTOR_ROTATION_PERIOD,
+                          EGG_QUAIL_TARGET_TEMP,
+                          EGG_QUAIL_TARGET_HUMD};
+
+inline egg_t duck_egg = {EGG_DUCK_TYPE,
+                         EGG_DUCK_INCUBATION_PERIOD,
+                         EGG_DUCK_MOTOR_ROTATION_PERIOD,
+                         EGG_DUCK_START_MOTOR_ROTATION_PERIOD,
+                         EGG_DUCK_STOP_MOTOR_ROTATION_PERIOD,
+                         EGG_DUCK_TARGET_TEMP,
+                         EGG_DUCK_TARGET_HUMD};
+
+inline egg_t turkey_egg = {EGG_TURKEY_TYPE,
+                           EGG_TURKEY_INCUBATION_PERIOD,
+                           EGG_TURKEY_MOTOR_ROTATION_PERIOD,
+                           EGG_TURKEY_START_MOTOR_ROTATION_PERIOD,
+                           EGG_TURKEY_STOP_MOTOR_ROTATION_PERIOD,
+                           EGG_TURKEY_TARGET_TEMP,
+                           EGG_TURKEY_TARGET_HUMD};
+
+inline egg_t goose_egg = {EGG_GOOSE_TYPE,
+                          EGG_GOOSE_INCUBATION_PERIOD,
+                          EGG_GOOSE_MOTOR_ROTATION_PERIOD,
+                          EGG_GOOSE_START_MOTOR_ROTATION_PERIOD,
+                          EGG_GOOSE_STOP_MOTOR_ROTATION_PERIOD,
+                          EGG_GOOSE_TARGET_TEMP,
+                          EGG_GOOSE_TARGET_HUMD};
+
+inline egg_t pigeon_egg = {EGG_PIGEON_TYPE,
+                           EGG_PIGEON_INCUBATION_PERIOD,
+                           EGG_PIGEON_MOTOR_ROTATION_PERIOD,
+                           EGG_PIGEON_START_MOTOR_ROTATION_PERIOD,
+                           EGG_PIGEON_STOP_MOTOR_ROTATION_PERIOD,
+                           EGG_PIGEON_TARGET_TEMP,
+                           EGG_PIGEON_TARGET_HUMD};
 
 #endif  // !EGG_H
 
