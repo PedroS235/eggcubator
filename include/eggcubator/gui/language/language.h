d771667be389886346e36530a8632b3df23dec6a
Pedro Soares <pmbs.123@gmail.com>
2023-06-24 12:07:13 +0200

    [Feat] Add support for english and portuguese languages

---
+++ include/eggcubator/gui/language/language.h
@@ -0,0 +1,64 @@
+/*
+ * EggCubator - Arduino-based egg incubator controller
+ * Copyright (C) 2023 Pedro Soares
+ * See end of the file for extended copyright information
+ */
+
+#ifndef LANGUAGE_H
+#define LANGUAGE_H
+
+#include <eggcubator/configuration.h>
+
+#ifdef LANGUAGE_PT
+#include "eggcubator/gui/language/language_pt.h"
+#else
+
+// English
+
+#define GO_BACK_STR "Go back"
+#define INCUBATE_STR "Incubation"
+#define PREHEAT_STR "Preheat"
+#define SETTINGS_STR "Settings"
+#define SAVE_STR "Save"
+#define RESET_STR "Reset"
+#define MANUAL_STR "Manual"
+#define CALIBRATE_STR "Calibrate"
+#define EGGS_SETTINGS_STR "Eggs settings"
+#define TUNE_STR "Tune"
+#define STOP_INCUBATION_STR "Stop incubation"
+#define TEMEPERATURE_STR "Temperature"
+#define HUMIDITY_STR "Humidity"
+#define MOTOR_DURATION_STR "Motor dur."
+#define TARGET_TEMPERATURE_STR "Target temp."
+#define TARGET_HUMIDITY_STR "Target humid."
+#define PERIOD_STR "Period"
+#define START_OF_ROTATION_STR "Start of rot."
+#define STOP_OF_ROTATION_STR "Stop of rot."
+#define ROTATION_PERIOD_STR "Rot. period"
+
+#define CHICKEN_STR "Chicken"
+#define QUAIL_STR "Quail"
+#define DUCK_STR "Duck"
+#define TURKEY_STR "Turkey"
+#define GOOSE_STR "Goose"
+#define PIGEON_STR "Pigeon"
+
+#endif
+
+#endif  // !LANGUAGE_H
+
+/*
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
---
+++ include/eggcubator/gui/language/language_pt.h
@@ -0,0 +1,54 @@
+/*
+ * EggCubator - Arduino-based egg incubator controller
+ * Copyright (C) 2023 Pedro Soares
+ * See end of the file for extended copyright information
+ */
+
+#ifndef LANGUAGE_PT_H
+#define LANGUAGE_PT_H
+
+#define GO_BACK_STR "Voltar"
+#define INCUBATE_STR "Incubação"
+#define PREHEAT_STR "Pré-aquecimento"
+#define SETTINGS_STR "Definições"
+#define SAVE_STR "Guardar"
+#define RESET_STR "Repor"
+#define MANUAL_STR "Manual"
+#define CALIBRATE_STR "Calibrar"
+#define EGGS_SETTINGS_STR "Def. dos ovos"
+#define TUNE_STR "Afinar"
+#define STOP_INCUBATION_STR "Parar incubação"
+#define TEMEPERATURE_STR "Temperatura"
+#define HUMIDITY_STR "Humidade"
+#define MOTOR_DURATION_STR "Dur. do motor"
+#define TARGET_TEMPERATURE_STR "Temp. alvo"
+#define TARGET_HUMIDITY_STR "Hum. alvo"
+#define PERIOD_STR "Período"
+#define START_OF_ROTATION_STR "Início de rot."
+#define STOP_OF_ROTATION_STR "Fim de rot."
+#define ROTATION_PERIOD_STR "Período de rot."
+
+#define CHICKEN_STR "Galinha"
+#define QUAIL_STR "Codorniz"
+#define DUCK_STR "Pato"
+#define TURKEY_STR "Peru"
+#define GOOSE_STR "Ganso"
+#define PIGEON_STR "Pombo"
+
+#endif  // !#ifndef LANGUAGE_PT_H
+
+/*
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
