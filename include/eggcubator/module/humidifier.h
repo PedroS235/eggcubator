48a9bff25774f16b1010c5c75a194ba5ebd38854
Pedro Soares <pmbs.123@gmail.com>
2023-07-14 22:58:07 +0200

    feat(modules): implement partially an humidifier

---
+++ include/eggcubator/module/humidifier.h
@@ -0,0 +1,120 @@
+/*
+ * EggCubator - Arduino-based egg incubator controller
+ * Copyright (C) 2023 Pedro Soares
+ * See end of the file for extended copyright information
+ */
+
+#ifndef HUMIDIFIER_H
+#define HUMIDIFIER_H
+
+#include <DHT.h>
+
+#include "eggcubator/configuration.h"
+#include "eggcubator/module/pid.h"
+
+class Humidifier {
+   private:
+    float humidity;
+    float humidity_correction;
+    float humidity_target;
+    float prev_humidity_target;
+    unsigned long last_humidity_reading_time;
+    unsigned long humidity_reading_interval;
+
+    PID* pid;
+    DHT* humidity_sensor;
+
+   private:
+    void update_humidity();
+
+   public:
+    Humidifier(DHT* dht_sensor,
+               unsigned long humidity_reading_interval_ = 250,
+               float humidity_correction_ = 0);
+
+    /**
+     * @brief Method to return the current humidity reading
+     *
+     * @returns Last humidity reading
+     */
+    float get_humidity();
+
+    /**
+     * @brief Method to update the current humidity correction
+     *
+     * @param new_correction is the new correction value to be applied
+     */
+    void set_humidity_correction(float new_correction);
+    float get_humidity_correction();
+
+    /**
+     * @brief Method to set the current humidity target the thermostat
+     * should aim for
+     *
+     * @param new_target is the new humidity target that should be applied
+     *
+     */
+    void set_humidity_target(float new_target);
+
+    /**
+     * @brief Updates the current proportional term of the PID controller
+     *
+     * @param new_p is the new proportional term to be set
+     */
+    void update_pid_p_term(float new_p);
+
+    /**
+     * @brief Updates the current proportional term of the PID controller
+     *
+     * @param new_p is the new proportional term to be set
+     */
+    void update_pid_i_term(float new_i);
+
+    /**
+     * @brief Updates the current proportional term of the PID controller
+     *
+     * @param new_p is the new proportional term to be set
+     */
+    void update_pid_d_term(float new_d);
+
+    /**
+     * @brief Updates the current PID term values of the PID controller
+     *
+     * @param new_p is the new proportional term to be set
+     * @param new_i is the new integral term to be set
+     * @param new_d is the new derivative term to be set
+     */
+    void update_pid_terms(float new_p, float new_i, float new_d);
+    void update_pid_terms(pid_terms_t new_terms);
+    pid_terms_t get_pid_terms();
+
+    /**
+     * @brief Main method that should run continuously.
+     * It is responsible of reading the temperature and controlling the heating
+     * source in order to maintain the targeted temperature
+     *
+     * @param temp_target is the targeted temperature that the thermostat should
+     * aim for
+     *
+     * @return True if temperature sensor is working as expected, false otherwise
+     */
+    bool routine(float humidity_target_);
+};
+
+#endif  // !HUMIDIFIER_H
+
+/*
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
---
+++ src/eggcubator/module/humidifier.cpp
@@ -0,0 +1,74 @@
+#include "eggcubator/module/humidifier.h"
+
+#include "eggcubator/pins.h"
+
+Humidifier::Humidifier(DHT *dht_sensor,
+                       unsigned long humidity_reading_interval_,
+                       float humidity_correction_)
+    : humidity(NAN),
+      humidity_target(0),
+      prev_humidity_target(0),
+      last_humidity_reading_time(0) {
+    humidity_sensor = dht_sensor;
+    pid = new PID(PID_SERVO_KP, PID_SERVO_KI, PID_SERVO_KD);
+
+    humidity_reading_interval = humidity_reading_interval_;
+    humidity_correction = humidity_correction_;
+}
+
+void Humidifier::update_humidity() {
+    unsigned long now = millis();
+    if (now - last_humidity_reading_time >= humidity_reading_interval) {
+        const float reading = humidity_sensor->readHumidity();
+        if (!isnan(reading)) {
+            humidity = reading + humidity_correction;
+        } else {
+            humidity = NAN;
+        }
+        last_humidity_reading_time = now;
+    }
+}
+
+float Humidifier::get_humidity() { return humidity; }
+
+void Humidifier::set_humidity_correction(float new_correction) {
+    humidity_correction = new_correction;
+}
+
+float Humidifier::get_humidity_correction() { return humidity_correction; }
+
+void Humidifier::set_humidity_target(float new_target) { humidity_target = new_target; }
+
+void Humidifier::update_pid_p_term(float new_p) { pid->update_p_term(new_p); }
+
+void Humidifier::update_pid_i_term(float new_i) { pid->update_i_term(new_i); }
+
+void Humidifier::update_pid_d_term(float new_d) { pid->update_d_term(new_d); }
+
+void Humidifier::update_pid_terms(float new_p, float new_i, float new_d) {
+    pid->update_pid_terms(new_p, new_i, new_d);
+}
+
+void Humidifier::update_pid_terms(pid_terms_t new_pid_terms) {
+    pid->update_pid_terms(new_pid_terms);
+}
+
+pid_terms_t Humidifier::get_pid_terms() { return pid->get_pid_terms(); }
+
+bool Humidifier::routine(float humidity_target) {
+    update_humidity();
+
+    if (isnan(humidity)) {
+        return false;
+    }
+
+    // Reset PID in case the temperature target changes
+    if (prev_humidity_target != humidity_target) {
+        pid->reset();
+        prev_humidity_target = humidity_target;
+    }
+
+    // Calculate PID output for servo
+
+    return true;
+}
